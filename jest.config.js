9fdb4dc81e39e0ed964832f9bd0d40cd812d99c9
João Humberto <joaohbmenezes@outlook.com>
2023-03-29 10:56:51 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -22,5 +22,7 @@ module.exports = {
   },
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: ['<rootDir>/src']
+  roots: ['<rootDir>/src'],
+
+  preset: '@shelf/jest-mongodb'
 }
